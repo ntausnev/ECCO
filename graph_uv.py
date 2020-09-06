cf96651d8be8acae777505f22363bf55c45e6978
tnl <ntausnev@gmail.com>
2020-09-06 09:31:14 -0400

    initial commit all work

---
+++ graph_uv.py
@@ -0,0 +1,89 @@
+def generate_plot(ax, plt, x_plot, y_ocng, y_ocnh, y_zero, \
+        line_g, line_h, \
+        legend_dstg, legend_dsth,y_ticks):
+    import numpy as np
+
+    # Generate the plot
+    ax.plot(x_plot, y_ocng, lw=1, label=legend_dstg, color="red")
+    ax.plot(x_plot, y_ocng, color="red", marker='o', ms=3.)
+    ax.plot(x_plot, y_zero, lw=0.5, color="black", linestyle="--")
+
+    ax.plot(x_plot, y_ocnh, lw=1, label=legend_dsth, color="green")
+    ax.plot(x_plot, y_ocnh, color="green", marker='o', ms=2.)
+
+    ax.text(0.25, -0.22, line_g, color='red',   transform=ax.transAxes)
+    ax.text(0.5,  -0.22, line_h, color='green', transform=ax.transAxes)
+
+    ax.set_ylabel('m / s')
+    # Make a plot with major ticks that are multiples of 20 and minor ticks that
+    # are multiples of 5.  Label major ticks with '%d' formatting but don't label
+    # minor ticks.
+    #ax.xaxis.set_major_locator(MultipleLocator(20))
+
+    # Set title for the plot
+#   ax.set_title(
+#       'E2.1 U Wind 10m Height Along Equator 2°x2° Monthly-Mean Jul2009',
+#       pad=25, fontweight="bold")
+
+    # Set up grid, legend, and limits
+    # Set up grid, legend, and limits
+    #ax.grid(True)
+    ax.grid(False)
+    ax.legend(frameon=False)
+
+    # For the major/minor ticks, use no labels; default NullFormatter.
+
+    ax.xaxis.set_major_locator(plt.MultipleLocator(30.))
+    ax.xaxis.set_minor_locator(plt.MultipleLocator(10.))
+    ax.yaxis.set_major_locator(plt.MultipleLocator(2.))
+    ax.yaxis.set_minor_locator(plt.MultipleLocator(1.))
+
+    x_ticks = np.arange(30., 420., 30.)
+#   y_ticks = np.arange(-8, 8, 2)
+
+    ax.set_xticklabels([
+        '$30^o$E', '$60^o$E', '$90^o$E', '$120^o$E', '$150^o$E', '$180^o$',
+        '$150^o$W', '$120^o$W', '$90^o$W', '$60^o$W', '$30^o$W', '$0^o$',
+        '$30^o$E'
+    ])
+#   ax.set_yticklabels([-8, -6, -4, -2, 0, 2, 4, 6, 8])
+
+    ax.tick_params(which="minor",
+                   axis="x",
+                   direction="in",
+                   bottom=True,
+                   top=True,
+                   left=False,
+                   right=False)
+    ax.tick_params(which="minor",
+                   axis="y",
+                   direction="in",
+                   bottom=False,
+                   top=False,
+                   left=True,
+                   right=True)
+    ax.tick_params(which="major",
+                   axis="x",
+                   direction="in",
+                   bottom=True,
+                   top=True)
+    ax.tick_params(which="major",
+                   axis="y",
+                   direction="in",
+                   left=True,
+                   right=True)
+    ax.tick_params(axis="x", direction="in")
+    ax.tick_params(axis="y", direction="in")
+
+    ax.tick_params(labelbottom=True,
+                   labeltop=True,
+                   labelleft=True,
+                   labelright=True)
+
+    ax.tick_params(axis='x', which='major', pad=7)
+
+    ax.set_xticks(x_ticks)
+    ax.set_yticks(y_ticks)
+
+    ax.set_xlim(30., 390.)
+#   ax.set_ylim(-8., 6.0)
---
+++ tnl_netcdf.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+'''
+NAME
+    NetCDF with Python
+PURPOSE
+    To demonstrate how to read and write data with NetCDF files using
+    a NetCDF file from the NCEP/NCAR Reanalysis.
+    Plotting using Matplotlib and Basemap is also shown.
+PROGRAMMER(S)
+    Chris Slocum
+REVISION HISTORY
+    20140320 -- Initial version created and posted online
+    20140722 -- Added basic error handling to ncdump
+                Thanks to K.-Michael Aye for highlighting the issue
+REFERENCES
+    netcdf4-python -- http://code.google.com/p/netcdf4-python/
+    NCEP/NCAR Reanalysis -- Kalnay et al. 1996
+        http://dx.doi.org/10.1175/1520-0477(1996)077<0437:TNYRP>2.0.CO;2
+'''
+import os
+import matplotlib.pyplot as plt
+
+
+def ncdump(nc_fid, verb=True):
+    '''
+    ncdump outputs dimensions, variables and their attribute information.
+    The information is similar to that of NCAR's ncdump utility.
+    ncdump requires a valid instance of Dataset.
+
+    Parameters
+    ----------
+    nc_fid : netCDF4.Dataset
+        A netCDF4 dateset object
+    verb : Boolean
+        whether or not nc_attrs, nc_dims, and nc_vars are printed
+
+    Returns
+    -------
+    nc_attrs : list
+        A Python list of the NetCDF file global attributes
+    nc_dims : list
+        A Python list of the NetCDF file dimensions
+    nc_vars : list
+        A Python list of the NetCDF file variables
+    '''
+    def print_ncattr(key):
+        """
+        Prints the NetCDF file attributes for a given key
+
+        Parameters
+        ----------
+        key : unicode
+            a valid netCDF4.Dataset.variables key
+        """
+        try:
+            print("\t\ttype:", repr(nc_fid.variables[key].dtype))
+            for ncattr in nc_fid.variables[key].ncattrs():
+                print('\t\t%s:' % ncattr,
+                      repr(nc_fid.variables[key].getncattr(ncattr)))
+        except KeyError:
+            print("\t\tWARNING: %s does not contain variable attributes" % key)
+
+    # NetCDF global attributes
+    nc_attrs = nc_fid.ncattrs()
+    if verb:
+        print("NetCDF Global Attributes:")
+        for nc_attr in nc_attrs:
+            print('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
+    nc_dims = [dim for dim in nc_fid.dimensions]  # list of nc dimensions
+    # Dimension shape information.
+    if verb:
+        print("NetCDF dimension information:")
+        for dim in nc_dims:
+            print("\tName:", dim)
+            print("\t\tsize:", len(nc_fid.dimensions[dim]))
+            print_ncattr(dim)
+    # Variable information.
+    nc_vars = [var for var in nc_fid.variables]  # list of nc variables
+    if verb:
+        print("NetCDF variable information:")
+        for var in nc_vars:
+            if var not in nc_dims:
+                print('\tName:', var)
+                print("\t\tdimensions:", nc_fid.variables[var].dimensions)
+                print("\t\tsize:", nc_fid.variables[var].size)
+                print_ncattr(var)
+    return nc_attrs, nc_dims, nc_vars
+
+
+def save(path, ext='png', close=True, verbose=True):
+    """Save a figure from pyplot.
+    Parameters
+    ----------
+    path : string
+        The path (and filename, without the extension) to save the
+        figure to.
+    ext : string (default='png')
+        The file extension. This must be supported by the active
+        matplotlib backend (see matplotlib.backends module).  Most
+        backends support 'png', 'pdf', 'ps', 'eps', and 'svg'.
+    close : boolean (default=True)
+        Whether to close the figure after saving.  If you want to save
+        the figure multiple times (e.g., to multiple formats), you
+        should NOT close it in between saves or you will have to
+        re-plot it.
+    verbose : boolean (default=True)
+        Whether to print information about when and where the image
+        has been saved.
+    """
+
+    # Extract the directory and filename from the given path
+    directory = os.path.split(path)[0]
+    filename = "%s.%s" % (os.path.split(path)[1], ext)
+    if directory == '':
+        directory = '.'
+
+    # If the directory does not exist, create it
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # The final path to save to
+    savepath = os.path.join(directory, filename)
+
+    if verbose:
+        print(("Saving figure to '%s'..." % savepath), end=' ')
+
+    # Actually save the figure
+    plt.savefig(savepath)
+
+    # Close it
+    if close:
+        plt.close()
+
+    if verbose:
+        print("Done")
